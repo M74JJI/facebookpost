097ef57ac350f6557f2bab5d41d72a6988fd6203
MOHAMED HAJJI <pytaek1@gmail.com>
2022-01-11 20:23:43 +0100

    final

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var like_icon = document.getElementById('like_icon');
 var love_icon = document.getElementById('love_icon');
 var haha_icon = document.getElementById('haha_icon');
 var sad_icon = document.getElementById('sad_icon');
+var wow_icon = document.getElementById('wow_icon');
 var angry_icon = document.getElementById('angry_icon');
 var img_react = document.getElementById('img_react');
 var text_react = document.getElementById('text_react');
 var reacts = document.querySelector('.reacts');
 var like_btn = document.querySelector('.like_btn');
 var count_com = document.querySelector('.count_com');
+var input_comment = document.querySelector('.input_comment');
+var comments = document.querySelector('.comments');
 var react_type = '';
 var clicked = false;
 function react(type) {
@@ -56,6 +59,13 @@ haha_icon.addEventListener('click', function () {
         react('haha');
     }
 });
+wow_icon.addEventListener('click', function () {
+    if (react_type == 'wow') {
+        Removereact('wow');
+    } else {
+        react('wow');
+    }
+});
 sad_icon.addEventListener('click', function () {
     if (react_type == 'sad') {
         Removereact('sad');
@@ -68,5 +78,20 @@ angry_icon.addEventListener('click', function () {
         Removereact('angry');
     } else {
         react('angry');
+    }
+});
+var count = 0;
+input_comment.addEventListener('keyup', function (e) {
+    if (e.keyCode == 13) {
+        var comment = this.value;
+        if (comment != '') {
+            var template =
+                ' <div class="comment"> <img src="images/profile.jpeg" alt="" class="com_img"> <div class="com_wrap"> <div class="col"> <div class="com_name">dev7 channel</div> <div class="com_text">' +
+                comment +
+                '</div> </div> </div> </div>';
+            comments.insertAdjacentHTML('beforeend', template);
+            this.value = '';
+            count_com.innerText = ++count + ' comments';
+        }
     }
 });
